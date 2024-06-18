0ff7951abd6b375a205c4ed89d25b97b5ce8e826
Juandy <84140394+Juandy36@users.noreply.github.com>
2024-06-18 18:55:04 -0500

    Create recicladora.py

---
+++ recicladora.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+import random
+import os
+import requests 
+
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+# Define intents
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+# Crea un bot con el prefijo deseado
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+# Evento on_ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+
+
+bot.run('MTI1Mjc2NzE5NTg3NjgxODk4NQ.GZ8SnJ.lNhlT7cxeD27LZbKKGeNwGc6B14yPSEiovaUtY')
